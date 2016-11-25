6c456bb4111a34840964f9b8af2e7e6b93d4fdb9
simon2m <simon2m@o2.pl>
2016-11-25 08:41:55 +0100

    Add files via upload

---
+++ Lab56/src/main/java/facebookapp/FacebookApp.java
@@ -0,0 +1,17 @@
+package main.java.facebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class FacebookApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class, args);
+	}
+
+}
